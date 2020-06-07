f53b2b586d69887e2c20e4fcf7c91696305416a0
sarumathiramasamy <43061815+sarumathiramasamy@users.noreply.github.com>
2020-06-07 23:37:51 +0800

    Updates

---
+++ precache-manifest.77ba40f688a1644d0e9ec0ee0acfaff1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6baf4939902e250716fb3856aabe0e",
+    "url": "/ReactStyled/index.html"
+  },
+  {
+    "revision": "f3bd9d90b044f5c856cd",
+    "url": "/ReactStyled/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "8b3b5bde18bf52692312",
+    "url": "/ReactStyled/static/js/2.9df1da3e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactStyled/static/js/2.9df1da3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3bd9d90b044f5c856cd",
+    "url": "/ReactStyled/static/js/main.82bf379c.chunk.js"
+  },
+  {
+    "revision": "c3aa2d250bb9392229e3",
+    "url": "/ReactStyled/static/js/runtime-main.9cc335cb.js"
+  },
+  {
+    "revision": "d61546cda03af82f672458cb776c67a1",
+    "url": "/ReactStyled/static/media/boatImage.d61546cd.jpg"
+  }
+]);
